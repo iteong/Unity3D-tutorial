b70197c939d701cc55356ac63ff97bb6e234bdfb
Ivan Teong <ivanteongsh@gmail.com>
2018-08-25 18:31:19 +1000

    Initial welcome screen for hacking

---
+++ 2_Terminal_Hacker/2_Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Console initialised!");
+        Terminal.WriteLine("Welcome to Ivan's World!");
+        Terminal.WriteLine("Where do you want to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Enter your selection:");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
