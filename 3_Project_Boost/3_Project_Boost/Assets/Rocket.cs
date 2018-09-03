f3c67cc149a9c9d563a52e28bc4626b7473e91b0
Ivan Teong <ivanteongsh@gmail.com>
2018-09-03 21:08:06 +1000

    Added input movements for rocket

---
+++ 3_Project_Boost/3_Project_Boost/Assets/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidBody.AddRelativeForce(Vector3.left);
+        } 
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidBody.AddRelativeForce(Vector3.right);
+        } 
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidBody.AddRelativeForce(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidBody.AddRelativeForce(Vector3.back);
+        }
+    }
+}
